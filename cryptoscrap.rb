89f110b19e44599db2d627d0523af8bd2b41d101
Jules <rigomontj@gmail.com>
2018-01-18 22:47:56 +0100

    commentaires et indentation

--- cryptoscrap.rb
+++ cryptoscrap.rb
@@ -1,7 +1,7 @@
 require "nokogiri"
 require "open-uri"
 
-$tiime = "0"
+$tiime = "0" #variable globale égale au temps d'attente entré au lancement du programme 
 
 def get_info
 array = []
@@ -10,33 +10,34 @@ i = 0
 j = 0
 doc = Nokogiri::HTML(open('https://coinmarketcap.com/'))
 
-doc.css('a.currency-name-container').each do |name|
+doc.css('a.currency-name-container').each do |name| #les noms de cryptos
 	File.open('crypto_db_names.txt', 'a') { |file| 
-	file.puts(name.text)
+	file.puts(name.text) #feedback de ce que le programme est entrain d'importer
 	}
 end
 
-File.readlines('crypto_db_names.txt').each do |line|
-        array[i] = line[0..-2]
+File.readlines('crypto_db_names.txt').each do |line| #les noms de cryptos qui sont dans le fichier texte
+        array[i] = line[0..-2] #enregistrer le nom(sans le '\n') dans un tableau
         i = i + 1
 end
 
-doc.css('a.price').each do |usd|
+doc.css('a.price').each do |usd| #les prix de cryptos
 	File.open('crypto_db_usd.txt', 'a') { |file| 
-	file.puts("$" + usd['data-usd'])
+	file.puts("$" + usd['data-usd']) #feeback de ce que le programme est entrain d'importer
 	}
 end
 
-File.readlines('crypto_db_usd.txt').each do |line|
-        db[(array[j]).to_sym] = line[0..-2]
+File.readlines('crypto_db_usd.txt').each do |line| #pour chaque ligne du fichier texte avec les prix de crypto
+        db[(array[j]).to_sym] = line[0..-2] #on associe des keys(nomdelacrypto) avec leurs valeurs(priceusd)
         j = j + 1
 end
 
-return db
+return db #return le hash
 
 end
 
 def perform
+	#prompt de temps avant le prochain refresh
 	if $tiime == "0"
 		puts "How long do you want to wait between each price update ? :"
 		$tiime = gets.chomp
@@ -46,7 +47,7 @@ def perform
 	sleep(1)
 	puts "=>  Waiting #{$tiime}s before updating cryptocurrency prices"
 	sleep($tiime.to_i)
-	perform
+	perform #lance le programme
 end
 
 perform
--- mairies.rb
+++ mairies.rb
@@ -1,30 +1,32 @@
-#On ajoute les librairies open-uri et nokogiri
 require "open-uri"
 require "nokogiri"
 
-def get_info
-addresses = []
-db = Hash.new
-i = 0
-j = 0
-puts "Veuillez entrer une addresse de page de type 'http://annuaire-des-mairies.com/departement.html' :"
+def get_info #fonction de scraping
+  addresses = []
+  db = Hash.new
+  i = 0
+  j = 0
+#prompt de departement
+  puts "Veuillez entrer une addresse de page de type 'http://annuaire-des-mairies.com/departement.html' :"
   urldpt = gets.chomp
   doc = Nokogiri::HTML(open(urldpt))
-  doc.css('p a.lientxt').each do |children|
+  doc.css('p a.lientxt').each do |children| #importation de toutes les mairies du departement
       addresses[i] =  "http://annuaire-des-mairies.com" + children["href"][1..-1]
       puts "importing:  " + addresses[i]
       i = i + 1
   end
-for j in 0...addresses.length
- doc2 = Nokogiri::HTML(open(addresses[j])) 
- doc2.css('tr/td/p/font').each do |children| 
- if /@/ =~ children.text
-	puts "importing:  " + children.text[1..-1] 
- 	db[(addresses[j])[35..-6]] = children.text[1..-1]
- end 
- end
- end 
- return db
+
+for j in 0...addresses.length #importation de l'addresse email de chaque mairie
+  doc2 = Nokogiri::HTML(open(addresses[j])) 
+  doc2.css('tr/td/p/font').each do |children| 
+    if /@/ =~ children.text
+      puts "importing:  " + children.text[1..-1] 
+      db[(addresses[j])[35..-6]] = children.text[1..-1]
+    end 
+  end
+end 
+
+return db
 end
 
 def perform
